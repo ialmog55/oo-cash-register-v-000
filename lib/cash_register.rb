e0fb406e0f7cff1c3b0549915b3e74e549ec174c
idith almog <almog.idith@gmail.com>
2020-05-17 00:09:13 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -13,6 +13,7 @@ class CashRegister
       return "There is no discount to apply."
     else
       @total -= @total * @discount / 100
+      return "After the discount, the total comes to $#{@total}."
     end
   end
 end
