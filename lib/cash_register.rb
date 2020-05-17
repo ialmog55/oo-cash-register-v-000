67834fa3fc434ea2862d871bb8b49cc0a2232bac
idith almog <almog.idith@gmail.com>
2020-05-17 00:12:14 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,12 +1,16 @@
 class CashRegister
   attr_accessor :total
   attr_reader :discount
+  @@items = []
   def initialize(discount=nil)
     @total = 0
     @discount = discount
   end
   def add_item(title,price,quantity = 1)
     @total += price * quantity
+    quantity.times do 
+      @@items << title
+    end
   end
   def apply_discount
     if @discount == nil
@@ -15,5 +19,9 @@ class CashRegister
       @total -= @total * @discount / 100
       return "After the discount, the total comes to $#{@total}."
     end
+  end
+
+  def items
+    @@items
   end
 end
