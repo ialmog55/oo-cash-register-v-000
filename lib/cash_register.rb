61920fd56fab5d512661b72d7d9c487e21243379
idith almog <almog.idith@gmail.com>
2020-05-17 00:07:58 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -7,6 +7,11 @@ class CashRegister
   end
   def add_item(title,price,quantity = 1)
     @total += price * quantity
-
+  end
+  def apply_discount
+    if @discount == nil
+      return "There is no discount to apply."
+    else
+      @total -= @total * @discount / 100
   end
 end
